c5d5721336b142e8990fc092683d0c6741411aac
abbbabb736-star <abbbabb736@gmail.com>
2026-04-05 20:50:21 +0500

    fix bugs

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import StatisticsChart from './components/StatisticsChart';
+import HomePage from './pages/HomePage'
+import ProductDetail from './pages/ProductDetail'
+import CartPage from './pages/CartPage'
+import CheckoutPage from './pages/CheckoutPage'
+
+import AdminLogin from './pages/AdminLogin'
+import AdminProducts from './pages/AdminProducts'
+import AdminOrders from './pages/AdminOrders'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <main className="app-shell">
+        
+        {/* AGAR CHART HAMMA SAHIFADA CHIQISHI KERAK BO'LSA, SHU YERGA QO'YING */}
+        <div style={{ padding: '20px' }}>
+             <h1>Tizim statistikasi</h1>
+             <div style={{ height: '400px', marginTop: '20px' }}>
+                 <StatisticsChart />
+             </div>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          
+          <Route
+            path="/admin/products"
+            element={
+              <ProtectedRoute>
+                <AdminProducts />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/admin/orders"
+            element={
+              <ProtectedRoute>
+                <AdminOrders />
+              </ProtectedRoute>
+            }
+          />
+          
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  )
+}
+
+export default App
